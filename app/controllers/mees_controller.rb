6451907ce5873fa7c5dc4ef25d94afdd7004bd67
kijima-line <marugo2792@gmail.com>
2022-04-28 02:39:33 +0900

    未実装の実装

--- app/controllers/mees_controller.rb
+++ app/controllers/mees_controller.rb
@@ -4,15 +4,32 @@ class MeesController < ApplicationController
   end
 
   def new
+    @mee = Mee.new
+    
   end
+
 
   def create
+    mee = Mee.new(mee_params)
+    if mee.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  def destroy
+    mee = Mee.find(params[:id])
+    if mee.destroy
+      redirect_to root_path, notice: '削除しました。'
+    else
+      render :index
+    end
+  end
 
   private
   def mee_params
-    params.require(:mee).permit(:q_1, :q_2, :q_3, :q_4, :q_5, :q_6, :q_7).merge(user_id: current_user.id)
+    params.require(:mee).permit(:q_1, :q_2, :q_3, :q_4, :q_5, :q_6, :q_7)
   end
-  
+  #.merge(user_id: current_user.id)
 end
