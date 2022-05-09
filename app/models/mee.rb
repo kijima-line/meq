4d17cc15dbe9f40b6b5e66fcf9d1358709078c72
jun <97328580+kijima-line@users.noreply.github.com>
2022-05-09 20:46:41 +0900

    アップロード

--- app/models/mee.rb
+++ app/models/mee.rb
@@ -2,6 +2,7 @@ class Mee < ApplicationRecord
     belongs_to :user
     has_one_attached :image
     validates :q_1,presence: true
+    validates :image, presence: true
     
 
     validates :q_a,presence: true, allow_blank: true, numericality: {only_integer: true}, length: { in: 1..12 }
